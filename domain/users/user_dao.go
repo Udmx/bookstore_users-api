454701fad7952307fc6fbed41c5ecb5b86cad23e
udmx <pouriashafieeit@gmail.com>
2021-11-08 22:57:39 +0330

    Implementation UpdateUserAPI(PUT Method)

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -10,8 +10,9 @@ import (
 )
 
 const (
-	queryInsertUser  = "INSERT INTO users(first_name,last_name,email,date_created) VALUES(?,?,?,?);"
-	queryGetUser     = "SELECT id,first_name,last_name,email,date_created FROM users WHERE id=?;"
+	queryInsertUser = "INSERT INTO users(first_name,last_name,email,date_created) VALUES(?,?,?,?);"
+	queryGetUser    = "SELECT id,first_name,last_name,email,date_created FROM users WHERE id=?;"
+	queryUpdateUser = "UPDATE users SET first_name=?,last_name=?,email=? WHERE id=?;"
 )
 
 func (user *User) Get() *errors.RestErr {
@@ -48,5 +49,19 @@ func (user *User) Save() *errors.RestErr {
 	}
 
 	user.Id = userId
+	return nil
+}
+
+func (user *User) Update() *errors.RestErr {
+	stmt, err := users_db.Client.Prepare(queryUpdateUser)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
+	}
+	defer stmt.Close()
+
+	_, _err := stmt.Exec(user.FirstName, user.LastName, user.Email, user.Id)
+	if _err != nil {
+		return mysql_utils.ParseError(err)
+	}
 	return nil
 }
--- services/users_service.go
+++ services/users_service.go
@@ -17,12 +17,24 @@ func CreateUser(user users.User) (*users.User, *errors.RestErr) {
 	if err := user.Validate(); err != nil {
 		return nil, err
 	}
-	//if err := users.Validate(&user); err != nil {
-	//	return nil, err
-	//}
-
 	if err := user.Save(); err != nil {
 		return nil, err
 	}
 	return &user, nil
+}
+
+func UpdateUser(user users.User) (*users.User, *errors.RestErr) {
+	current, err := GetUser(user.Id)
+	if err != nil {
+		return nil, err
+	}
+
+	current.FirstName = user.FirstName
+	current.LastName = user.LastName
+	current.Email = user.Email
+
+	if err := current.Update(); err != nil {
+		return nil, err
+	}
+	return current, nil
 }
