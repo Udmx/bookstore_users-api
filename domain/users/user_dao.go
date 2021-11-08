53102f147e4438866dccde8f104145ec871b87fc
udmx <pouriashafieeit@gmail.com>
2021-11-08 15:48:08 +0330

    Implementation GetUserAPI

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -12,27 +12,49 @@ import (
 
 const (
 	indexUniqueEmail = "users_email_uindex"
+	errorNoRows      = " no rows in result set"
 	queryInsertUser  = "INSERT INTO users(first_name,last_name,email,date_created) VALUES(?,?,?,?);"
+	queryGetUser     = "SELECT id,first_name,last_name,email,date_created FROM users WHERE id=?;"
 )
 
-var (
-	usersDB = make(map[int64]*User)
-)
+//var (
+//	usersDB = make(map[int64]*User)
+//)
 
 func (user *User) Get() *errors.RestErr {
-	if err := users_db.Client.Ping(); err != nil {
-		panic(err) //try to connect
+	//if err := users_db.Client.Ping(); err != nil {
+	//	panic(err) //try to connect
+	//}
+	//result := usersDB[user.Id]
+	//if result == nil {
+	//	return errors.NewNotFoundError(fmt.Sprintf("user %d not found", user.Id))
+	//}
+	//user.Id = result.Id
+	//user.FirstName = result.FirstName
+	//user.LastName = result.LastName
+	//user.Email = result.Email
+	//user.DateCreated = result.DateCreated
+	stmt, err := users_db.Client.Prepare(queryGetUser)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
 	}
+	defer stmt.Close()
 
-	result := usersDB[user.Id]
-	if result == nil {
-		return errors.NewNotFoundError(fmt.Sprintf("user %d not found", user.Id))
+	//results,_:=stmt.Query(user.Id) // Return rows
+	//if err != nil {
+	//	return errors.NewInternalServerError(err.Error())
+	//}
+	//defer results.Close()
+
+	result := stmt.QueryRow(user.Id) //Return single row
+	if err := result.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email, &user.DateCreated); err != nil {
+		if strings.Contains(err.Error(), errorNoRows) {
+			return errors.NewNotFoundError(fmt.Sprintf("user %d not found", user.Id))
+		}
+		fmt.Println(err)
+		return errors.NewInternalServerError(
+			fmt.Sprintf("error when trying to get user %d: %s", user.Id, err.Error()))
 	}
-	user.Id = result.Id
-	user.FirstName = result.FirstName
-	user.LastName = result.LastName
-	user.Email = result.Email
-	user.DateCreated = result.DateCreated
 
 	return nil
 }
